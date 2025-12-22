4a9156fe0c9dd19222c22a73e9f959e978726128
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-12-22 07:21:43 -0500

    updates

--- variables.tf
+++ variables.tf
@@ -1,25 +1,31 @@
 variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "key_name" {
-  description = "EC2 key pair name"
-  type        = "LUIT_Linux1_Keys"
+  description = "EC2 key pair name"  
+  type        = string
+  default     = "LUIT_Linux1_Keys"
 }
 
 variable "instance_type" {
+  type    = string
   default = "t3.micro"
 }
 
 variable "manager_count" {
+  type    = number
   default = 1
 }
 
 variable "worker_count" {
+  type    = number
   default = 2
 }
 
 variable "project_name" {
+  type    = string
   default = "swarm-ecommerce"
 }
