c1429693707ea42f381f514311928d9e4dbec58f
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2026-01-03 19:23:02 -0500

    updated

--- main.tf
+++ main.tf
@@ -168,6 +168,25 @@ resource "aws_security_group" "worker_sg" {
   }
 }
 
+########################
+# Amazon Linux 2 AMI
+########################
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
 #############################
 # EC2 Instances
 #############################
